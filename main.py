e0706edb2199f3668a4004ecb2f31796b1a124ad
jbarr256 <119154445+jbarr256@users.noreply.github.com>
2022-11-27 16:16:21 -0800

    Member A starts on merge_sort and adds comments

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # @arg arr The input list like object to be sorted
 # @arg cmp A compare function which takes two element in the array, 
-#          cmp(a,b)<0   if a should be placed before b,
-#          cmp(a,b)==0  if arr is still sorted after a and b are exchanged,
-#          cmp(a,b)>0   if a should be placed behind b.
+#          cmp(a,b)<0   if a should be placed before b, i.e. a is less then b
+#          cmp(a,b)==0  if arr is still sorted after a and b are exchanged, a and b are equal already
+#          cmp(a,b)>0   if a should be placed behind b. a is larger then b
 def multi_sort(arr, cmp, method="None"):
     if(method=="quick"):
         quick_sort(arr,cmp)
@@ -14,10 +14,19 @@ def multi_sort(arr, cmp, method="None"):
         print("invalid argument!")
 
 
-
+def cmp(a,b):
+    if a==b:
+        return 0
+    elif a < b:
+        return -1
+    else:
+        return 1
 
 # must be in-place sort
+#member A will solve the in-place merge_sort
 def merge_sort(arr,cmp):
+
+
     pass
 
 # must be in-place sort
