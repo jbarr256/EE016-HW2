6e98bcd3c92a83e0660179df9c405dec4e3b515b
jbarr256 <119154445+jbarr256@users.noreply.github.com>
2022-11-29 19:38:39 -0800

    Changing to regular mergesort and fixing cmp

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import Random, randint
+
 # @arg arr The input list like object to be sorted
 # @arg cmp A compare function which takes two element in the array, 
 #          cmp(a,b)<0   if a should be placed before b, i.e. a is less then b
@@ -13,48 +15,33 @@ def multi_sort(arr, cmp, method="None"):
     else:
         print("invalid argument!")
 
-
-def cmp(a,b):
-    if a == b:
-        return 0
-    elif a < b:
-        return -1
-    else:
-        return 1
-
 # must be in-place sort
 #Member A will solve the in-place merge_sort
 #it does so by solving using recursion that uses a set midpoint it solves first the left then right side and then merges the two
-def inplaceMerge(arr,left,mid,right):
-    currL = left
-    currR = mid + 1
-    #Brute force the merge by going through each element and testing if it is in the correct location.
-    # If it is in the correct location , which should hace the left side be equal to or less then the right,then movve on
-    #  but if not then shift all elements betweeen the points and swap the points. Repeat until it it reaches end of one side.
-    while(currL <= mid and currR <= right):
-        if(arr[currL] <= arr[currR]):
-            currL += 1
-        else:
-            tempVal = arr[currR]
-            currIn = currR
-            #shift to the right
-            while(currIn > currL):
-                arr[currIn] = arr[currIn-1]
-                currIn -= 1
-            #swap
-            arr[currL] = tempVal
-            currL,currR,mid += 1
 
 def merge_sort(arr,cmp):
-    def inplaceMergeSort(arr,cmp,left,right):
-        if cmp == 0: return
-        mid = (left+right)//2
-        inplaceMergeSort(arr,cmp,left,mid)
-        inplaceMergeSort(arr,cmp,mid+1,right)
-        inplaceMerge(arr,left,mid,right)
-    #Call Function. No need to return anything
-    inplaceMergeSort(arr,cmp,arr[0],arr[len(arr)-1])
-    pass
+    if cmp(len(arr),1) <= 0:
+        return
+    mid = len(arr)//2
+
+    left = merge_sort(arr[:mid],cmp)
+    right = merge_sort(arr[mid:],cmp)
+    merged_list = []
+    i,j = 0
+    while i < len(left) and j < len(right):
+        if cmp(left[i],right[j]) > 1:
+            merged_list.append(left)
+            i += 1
+        else:
+            merged_list.append(right[j])
+            j += 1
+    while i < len(left):
+        merged_list.append(left[i])
+        i += 1
+    while j < len(right):
+        merged_list.append(right[j])
+        j += 1
+    return merged_list
 
 # must be in-place sort
 #Member B wil solve in place quicksort
@@ -63,14 +50,14 @@ def merge_sort(arr,cmp):
 # to know if it 
 def quick_sort(arr,cmp):
     def inplacequicksort(arr,cmp,left,right):
-        if cmp >= 0: return
+        if cmp(left,right) >= 0: return
         pivot = randint(0,len(arr)-1)
         currLeft = left
         currRight = right -1
-        while currLeft <= currRight:
-            while currLeft <= currRight and arr[currLeft] < pivot:
+        while currLeft <= currRight: #currLeft <= currRight:
+            while currLeft <= currRight and cmp(arr[currLeft],pivot) < 0:
                 currLeft += 1
-            while currLeft <= currRight and pivot < arr[currRight]:
+            while currLeft <= currRight  and cmp(arr[currRight],pivot) > 0:
                 currRight -= 1
             if currLeft <= currRight:
                 arr[currLeft],arr[currRight] = arr[currRight],arr[currLeft]
@@ -78,5 +65,6 @@ def quick_sort(arr,cmp):
         arr[currLeft],arr[right] = arr[right], arr[left]
         inplacequicksort(arr,left,currLeft-1)
         inplacequicksort(arr,currLeft + 1,right)
+
     inplacequicksort(arr,cmp,arr[0],arr[len(arr)-1])
     pass
